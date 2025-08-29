32896737d55c8053df069777f71c4e868045ab16
ChihaoDEV2025 <chihao.dev@gmail.com>
2025-08-29 10:27:55 +0700

    the further code in Python Programming

---
+++ Assignment/five.py
@@ -0,0 +1,1 @@
+print('My name: Nguyen Van Chi Hao')
---
+++ Assignment/seven.py
@@ -0,0 +1,6 @@
+import sys
+
+if len(sys.argv) > 1:
+    print(sys.argv[1])
+else:
+    print("Vui lòng nhập chuỗi ký tự qua tham số dòng lệnh.")
